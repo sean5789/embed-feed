6f1fed005b327e65561b7eed52c2b10ec1ae267c
sean <seanliccardi2001@gmail.com>
2025-07-08 02:23:00 +0200

    🔇 Clic vidéo ou bouton = son unique

--- generateStaticFeed.js
+++ generateStaticFeed.js
@@ -88,7 +88,7 @@ async function generateStaticFeed() {
 
     .card {
       flex: 0 0 auto;
-      width: 280px;
+      width: 200px;
       scroll-snap-align: start;
       background: white;
       border-radius: 16px;
