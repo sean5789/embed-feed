8eb53ed1f8c4f5ad9bdc46a5bcf90cc98bf0a154
sean <seanliccardi2001@gmail.com>
2025-12-24 02:47:53 +0100

    🔇 Clic vidéo ou bouton = son unique

--- generateStaticFeed.js
+++ generateStaticFeed.js
@@ -39,9 +39,9 @@ async function generateStaticFeed() {
           }
         </div>
         <div class="info">
-          <div class="date"> En 2025 ! ✈️🌍</div>
+          <div class="date"> En 2025 ! ✈️🌎</div>
           <div class="tag">
-            <a href="${CAL_URL}" target="_blank" rel="noopener noreferrer">✈️🥳🥂🎉➡️</a>
+            <a href="${CAL_URL}" target="_blank" rel="noopener noreferrer">✈️🌎🥳🥂🎉➡️</a>
           </div>
         </div>
       </div>
@@ -203,9 +203,9 @@ async function generateStaticFeed() {
         <div class="card">
           \${media}
           <div class="info">
-            <div class="date"> En 2025 ! ✈️🌍</div>
+            <div class="date"> En 2025 ! ✈️🌎</div>
             <div class="tag">
-              <a href="\${CAL_URL}" target="_blank" rel="noopener noreferrer">✈️🥳🥂🎉➡️</a>
+              <a href="\${CAL_URL}" target="_blank" rel="noopener noreferrer">✈️🌎🥳🥂🎉➡️</a>
             </div>
           </div>
         </div>\`;
