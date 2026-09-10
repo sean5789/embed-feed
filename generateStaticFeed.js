8e8b9acc8cb668769b215cb2b8415acc179dad81
sean5789 <seanliccardi2001@gmail.com>
2026-09-10 03:50:41 +0200

    Change touch-action from pan-x to pan-y

--- generateStaticFeed.js
+++ generateStaticFeed.js
@@ -81,7 +81,7 @@ async function generateStaticFeed() {
       overflow: hidden;
       padding: 0;
       box-sizing: border-box;
-      touch-action: pan-x;
+      touch-action: pan-y;
     }
 
     #stage {
@@ -681,6 +681,10 @@ async function generateStaticFeed() {
 
         manageVideoPlayback();
       });
+    });
+
+    window.addEventListener('resize', () => {
+      recalcAll();
     });
   </script>
 </body>
